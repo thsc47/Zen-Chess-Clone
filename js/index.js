7eb2f3d4f75888b3ddae00ce14c0047dbbd27f5c
Thiago Henrique <thsc47@gmail.com>
2021-06-24 20:04:14 -0300

    24/06 - classes individuais de cada célula criada automaticamente com o board

--- js/index.js
+++ js/index.js
@@ -3,16 +3,24 @@ class Board{
         let black = "<div class='chess-cell chess-cell-black'>";
         let white = "<div class='chess-cell chess-cell-white'>";
 
-        for (let row = 0; row < 8; row++) {
+        for (let row = 0; row < 8; row += 1) {
             let startWithBlack = row % 2;
-            for (let col = 0; col < 8; col++) {
+            for (let col = 0; col < 8; col += 1) {
                 let cellType = startWithBlack ? black : white;
                 startWithBlack = !startWithBlack;
                 e.innerHTML += cellType;
             }
         }
     }
+    cells() {
+        const cellsNode = document.querySelectorAll('.chess-cell')
+        for(let cells = 0; cells < 64; cells += 1) {
+            cellsNode[cells].classList.add(cells)
+        }
+    }
 }
+
 
 let board = new Board();
 board.draw(document.getElementById("chessboard"));
+board.cells()
