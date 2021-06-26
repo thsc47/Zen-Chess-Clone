b572818ba759f359d6471e63ae959239e2b9f160
Thiago Henrique <thsc47@gmail.com>
2021-06-26 11:06:09 -0300

    26/06 - Peças no tabuleiro, trabalhando na movimentação

--- js/board.js
+++ js/board.js
@@ -1,16 +1,17 @@
 //Classe para iniciar o board(vinculada ao futuro botão)
 class StartGame{
     constructor(){
-        let board = new Board();
+        const board = new Board();
+        const level = new Pieces();
         board.draw(document.getElementById("chessboard"));
-        board.cells()
+        level.LevelI();
     }
 }
 //Constroi o board e nomeia cada célula
 class Board{
     draw(e){
-        let black = "<div class='chess-cell chess-cell-black'>";
-        let white = "<div class='chess-cell chess-cell-white'>";
+        const black = "<div class='chess-cell chess-cell-black'>";
+        const white = "<div class='chess-cell chess-cell-white'>";
 
         for (let row = 0; row < 8; row += 1) {
             let startWithBlack = row % 2;
@@ -20,20 +21,34 @@ class Board{
                 e.innerHTML += cellType;
             }
         }
-    }
-    cells() {
-        const cellsNode = document.querySelectorAll('.chess-cell')
         for(let cells = 0; cells < 64; cells += 1) {
-            cellsNode[cells].classList.add(cells)
+            document.querySelectorAll('.chess-cell')[cells].classList.add('a'+cells)
         }
     }
 }
-//Inicia o nível I
-class Level{
-    constructor(board){
+//Insere as peças no Tabuleiro
+class Pieces {
+    LevelI() {
+        //Inserindo o Rei Negro
+        const blackKingCell = document.querySelector('.a4');
+        const blackKingImg = new Image();
+        blackKingImg.src = '../images/Black_king.png';
+        blackKingCell.appendChild(blackKingImg);
 
+        //inserindo o Rei Branco
+        const whiteKingCell = document.querySelector('.a21');
+        const whiteKingImg = new Image();
+        whiteKingImg.src = '../images/White_king.png';
+        whiteKingCell.appendChild(whiteKingImg);
+
+        //inserindo a Rainha Branca
+        const whiteQueenCell = document.querySelector('.a40');
+        const whiteQueenImg = new Image();
+        whiteQueenImg.src = '../images/White_queen.png'
+        whiteQueenCell.appendChild(whiteQueenImg);
     }
 }
+
 //Inicia o Board
 const startGame = document.querySelector('#start-game');
 startGame.addEventListener('click', () => {
